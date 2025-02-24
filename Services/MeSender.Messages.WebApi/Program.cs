f6babe8eb51d94160a8dde00723137e686e1ac21
Timur I.D. <timurid.dev@gmail.com>
2025-02-24 18:36:23 +0300

    Working version postgres based

--- Services/MeSender.Messages.WebApi/Program.cs
+++ Services/MeSender.Messages.WebApi/Program.cs
@@ -1,5 +1,11 @@
+using MeSender.Messages.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers(options => options.SuppressAsyncSuffixInActionNames = false);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ChatDbContext>(options => options.UseNpgsql(connectionString));
+
 var app = builder.Build();
 app.UseCors(policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 app.MapControllers();
--- Services/MeSender.Messages/Program.cs
+++ Services/MeSender.Messages/Program.cs
@@ -2,7 +2,6 @@ using MeSender.Messages.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ChatDbContext>(options => options.UseNpgsql(connectionString));
 
